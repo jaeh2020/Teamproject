932e3156bed6cdda5caa2f23c7f5ef6fd347d4cc
jiakimmm <blerang055@naver.com>
2021-09-23 00:04:29 +0900

    예약등록(예약자정보/주문등록) 화면 구현 시작

--- src/main/java/amdn/anywhere/controller/MainController.java
+++ src/main/java/amdn/anywhere/controller/MainController.java
@@ -3,6 +3,7 @@ package amdn.anywhere.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 
@@ -13,6 +14,27 @@ public class MainController {
 	public String main(Model model) {
 		model.addAttribute("title", "1팀 화이팅");
 		return "main";
+	}
+	
+	@GetMapping("/addBookMember")
+	public String getaddBookMember(Model model) {
+		model.addAttribute("addBookMember", "예약정보입력");
+		return "/book/addBookMember";
+	}
+	
+	@PostMapping("/addBookMember")
+		public String addBookMember(Model model) {
+			System.out.println("=============================");
+			System.out.println("dto만들어서 db에 담긴정보 나와야함");
+			System.out.println("=============================");
+
+		return "redirect:/book/addBookOrder";
+	}
+	
+	@GetMapping("/addBookOrder")
+	public String getaddBookOrder(Model model) {
+		model.addAttribute("addBookOrder", "주문정보입력");
+		return "/book/addBookOrder";
 	}
 	
 }
