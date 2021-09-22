1997cac7e460e5ccc9bf9df0bd4fa7b3c9ee47f7
codus3381 <codus3381@naver.com>
2021-09-23 01:08:13 +0900

    chaeyeon

---
+++ src/main/java/amdn/anywhere/mapper/BoardMapper.java
@@ -0,0 +1,13 @@
+package amdn.anywhere.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import amdn.anywhere.domain.Board;
+
+@Mapper
+public interface BoardMapper {
+	public List<Board> getBoardList();
+
+}
