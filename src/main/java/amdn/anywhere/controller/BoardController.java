3be1384281bf542fe52547e4d2ae498bb0e534d7
codus3381 <codus3381@naver.com>
2021-09-27 17:24:03 +0900

    [게시판 ajax 구현 완료 , 게시판 등록 구현 완료, 게시판 정보 구현 진행 중]

--- src/main/java/amdn/anywhere/controller/BoardController.java
+++ src/main/java/amdn/anywhere/controller/BoardController.java
@@ -1,9 +1,6 @@
 package amdn.anywhere.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.jar.Attributes.Name;
 
 import javax.servlet.http.HttpSession;
 
@@ -16,86 +13,109 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import amdn.anywhere.domain.Board;
 import amdn.anywhere.domain.BoardCate;
-import amdn.anywhere.domain.QuestionCate;
+import amdn.anywhere.domain.Member;
+import amdn.anywhere.domain.Statement;
 import amdn.anywhere.service.BoardService;
 
 @Controller
+/* @RequestMapping("/board") */
 public class BoardController {
-private final BoardService boardService;
-	
+	private final BoardService boardService;
+
 	public BoardController(BoardService boardService) {
 		this.boardService = boardService;
 	}
-
 	
-	
-
+	 // 게시글 수정
 	@GetMapping("/boardModify")
 	public String boardModify(Model model) {
 		model.addAttribute("title", "게시판 수정");
 		return "board/boardModify";
 	}
+
 	
 	
+	
+	
+	// 게시글 보기
 	@GetMapping("/boardView")
-	public String boardView(Model model) {
+	public String boardView(Model model
+			    			,@RequestParam(name = "boardNum" , required = false) String boardNum) {
+		
+	
+		
+		//게시물 정보 가져오기
+		Board board = boardService.getBoardInfoByCode(boardNum);
+		
+		
+		
 		model.addAttribute("title", "게시판 조회");
+		model.addAttribute("board", board);
+		
 		return "board/boardView";
 	}
+
 	
 	
+	  //ajax - 회원 정보 보기
+	  @GetMapping(value="/memberSearch", produces = "application/json")
+	  @ResponseBody 
+	  public Member getMemberList(@RequestParam(name="memberId", required = false) String memberId){ 		  
+		  Member member = boardService.getMemberRead(memberId);
+		  if(member == null) member = new Member();		  		  
+		  return member; 
+		  
+	  }
+	  
 	
-	// 회원정보보기 - ajax
-	@PostMapping(value="/memberSearch", produces = "application/json")
-	@ResponseBody
-	public Map<String,Object> getBoardList(@RequestParam("memberId") String memberId){
-		
-		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("memberId", memberId);
-		map.put("memberName", "홍길동");
-		map.put("memberAge", 20);
-				
-		return map;
-	}
-	
-	
+	  // 게시글 목록
 	@GetMapping("/boardList")
 	public String boardList(Model model) {
 		List<Board> boardList = boardService.getBoardList();
 		
+		
 		model.addAttribute("title", "게시판 목록");
 		model.addAttribute("location", "소비자 게시판");
 		model.addAttribute("boardList", boardList);
+		
 		return "board/boardList";
 	}
 	
-
+	 // 게시글 작성
 	@PostMapping("/boardWrite")
 	public String boardWrite(Board board) {
 		System.out.println("====================");
 		System.out.println("커맨드객체 board : " + board);
 		System.out.println("====================");
 		
-		if(board != null) boardService.boardWrite(board);
+
+		if (board != null)
+			boardService.boardWrite(board);
 		return "redirect:/boardList";
 	}
-	
-	
+
 	@GetMapping("/boardWrite")
 	public String reviewList(Model model
-	/*
-	 * ,@RequestParam(name = "boardCateCode" , required = false) String
-	 * boardCateCode
-	 */) {
+							,HttpSession session
+			,@RequestParam(name = "boardCateCode" , required = false) String boardCateCode
+			,@RequestParam(name = "boardStatementCode" , required = false) String boardStatementCode) {
 
+		//게시글 번호 가져오기
 		String newBoardNum = boardService.getNewBoardNum();
-		/* BoardCate boardCate = boardService.getBoardCateCode(boardCateCode); */
-		
+		//로그인 아이디 가져오기
+		String memberId = (String) session.getAttribute("SID");
+		//게시글 카테고리 코드 가져오기
+		BoardCate boardCate = boardService.getBoardCateCode(boardCateCode);
+		//상태코드 가져오기
+		Statement boardStatement = boardService.getboardStatement(boardStatementCode);
+
 		model.addAttribute("title", "게시판 등록");
-		model.addAttribute("boardNum" , newBoardNum);
-		/* model.addAttribute("boardCate" , boardCate); */
+		model.addAttribute("boardNum", newBoardNum);
+		model.addAttribute("memberId", memberId);
+		model.addAttribute("boardCate", boardCate);
+		model.addAttribute("boardStatement", boardStatement);
+		
 		return "board/boardWrite";
 	}
-	
-	
+
 }
--- src/main/java/amdn/anywhere/mapper/BoardMapper.java
+++ src/main/java/amdn/anywhere/mapper/BoardMapper.java
@@ -6,9 +6,26 @@ import org.apache.ibatis.annotations.Mapper;
 
 import amdn.anywhere.domain.Board;
 import amdn.anywhere.domain.BoardCate;
+import amdn.anywhere.domain.Member;
+import amdn.anywhere.domain.Statement;
 
 @Mapper
 public interface BoardMapper {
+	
+	
+	//게시물 정보 가져오기
+	public Board getBoardInfoByCode(String BoardNum);
+	//상태코드 가져오기
+	 public Statement getboardStatement(String boardStatementCode);
+	 
+	 //게시판 카테고리 코드 가져오기
+	 public BoardCate getBoardCateCode(String boardCateCode);
+	
+	//회원목록 조회
+	public List<Member> getMemberList();
+	
+	//아이디 조회
+	public Member getMemberRead(String memId);
 	
 	//게시판 등록
 	public int boardWrite(Board board);
@@ -19,7 +36,5 @@ public interface BoardMapper {
 	//게시판 목록
 	 public List<Board> getBoardList();
 	 
-	 //게시판 카테고리 코드 가져오기
-	 public BoardCate getBoardCateCode(String boardCateCode);
 	 
 }
