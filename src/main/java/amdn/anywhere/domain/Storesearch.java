732ce40b29ff44bf6044b95aeb7bafe25f97d947
ECS <jaeh2020>
2021-09-30 14:44:00 +0900

    [0930] 업데이트

---
+++ src/main/java/amdn/anywhere/domain/Storesearch.java
@@ -0,0 +1,106 @@
+package amdn.anywhere.domain;
+
+public class Storesearch {
+	private String storeCode;
+	private String storeName;
+	private String bizCode;
+	private String bizId;
+	private String storeLocation;
+	private String storeTime;
+	private String storePhone;
+	private String mainCateCode;
+	private String storeHoliday;
+	private String storeTasteAgree;
+	private String storePhoto;
+	private String storeTableNum;
+	private String storeOccupancy;
+	public String getStoreCode() {
+		return storeCode;
+	}
+	public void setStoreCode(String storeCode) {
+		this.storeCode = storeCode;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getBizCode() {
+		return bizCode;
+	}
+	public void setBizCode(String bizCode) {
+		this.bizCode = bizCode;
+	}
+	public String getBizId() {
+		return bizId;
+	}
+	public void setBizId(String bizId) {
+		this.bizId = bizId;
+	}
+	public String getStoreLocation() {
+		return storeLocation;
+	}
+	public void setStoreLocation(String storeLocation) {
+		this.storeLocation = storeLocation;
+	}
+	public String getStoreTime() {
+		return storeTime;
+	}
+	public void setStoreTime(String storeTime) {
+		this.storeTime = storeTime;
+	}
+	public String getStorePhone() {
+		return storePhone;
+	}
+	public void setStorePhone(String storePhone) {
+		this.storePhone = storePhone;
+	}
+	public String getMainCateCode() {
+		return mainCateCode;
+	}
+	public void setMainCateCode(String mainCateCode) {
+		this.mainCateCode = mainCateCode;
+	}
+	public String getStoreHoliday() {
+		return storeHoliday;
+	}
+	public void setStoreHoliday(String storeHoliday) {
+		this.storeHoliday = storeHoliday;
+	}
+	public String getStoreTasteAgree() {
+		return storeTasteAgree;
+	}
+	public void setStoreTasteAgree(String storeTasteAgree) {
+		this.storeTasteAgree = storeTasteAgree;
+	}
+	public String getStorePhoto() {
+		return storePhoto;
+	}
+	public void setStorePhoto(String storePhoto) {
+		this.storePhoto = storePhoto;
+	}
+	public String getStoreTableNum() {
+		return storeTableNum;
+	}
+	public void setStoreTableNum(String storeTableNum) {
+		this.storeTableNum = storeTableNum;
+	}
+	public String getStoreOccupancy() {
+		return storeOccupancy;
+	}
+	public void setStoreOccupancy(String storeOccupancy) {
+		this.storeOccupancy = storeOccupancy;
+	}
+	@Override
+	public String toString() {
+		return "Store [storeCode=" + storeCode + ", storeName=" + storeName + ", bizCode=" + bizCode + ", bizId="
+				+ bizId + ", storeLocation=" + storeLocation + ", storeTime=" + storeTime + ", storePhone=" + storePhone
+				+ ", mainCateCode=" + mainCateCode + ", storeHoliday=" + storeHoliday + ", storeTasteAgree="
+				+ storeTasteAgree + ", storePhoto=" + storePhoto + ", storeTableNum=" + storeTableNum
+				+ ", storeOccupancy=" + storeOccupancy + "]";
+	}
+	
+	
+	
+}
