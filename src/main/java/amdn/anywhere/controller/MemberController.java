28d0cf379c22d11787473d198300a8dc8da1f91d
Moon <ansdbal7@naver.com>
2021-10-04 23:43:34 +0900

    [회원가입 2단계까지 완료]

--- src/main/java/amdn/anywhere/controller/MemberController.java
+++ src/main/java/amdn/anywhere/controller/MemberController.java
@@ -1,11 +1,14 @@
 package amdn.anywhere.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PostMapping;
 
+import amdn.anywhere.domain.FoodMainCate;
 import amdn.anywhere.domain.Member;
 import amdn.anywhere.service.MemberService;
 
@@ -13,19 +16,37 @@ import amdn.anywhere.service.MemberService;
 @Controller
 public class MemberController {
 
+	private final MemberService memberService;
+	
+	public MemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
 
+	@GetMapping("/member/addMember03")
+	public String addMember03(Model model) {
+		
+		List<FoodMainCate> foodMainList = memberService.getFoodMainList();
+		
+		model.addAttribute("title", "회원가입  〉 추가정보입력");
+		model.addAttribute("location", "회원가입  〉 추가정보입력");
+		model.addAttribute("foodMainList", foodMainList);
+		
+		return "/member/addMember03";
+	}
+	
 	@PostMapping("/member/addMember02")
 	public String addMember02(Member member) {
 		System.out.println("커맨드 객체 : " + member);
 		
+		if(member != null) memberService.addMember02(member);
 		return "redirect:/member/addMember03";
 	}
 	
 	@GetMapping("/member/addMember02")
 	public String addMember02(Model model) {
 		
-		model.addAttribute("title", "회원가입  〉 회원유형선택");
-		model.addAttribute("location", "회원가입  〉 회원유형선택");
+		model.addAttribute("title", "회원가입  〉 정보입력");
+		model.addAttribute("location", "회원가입  〉 정보입력");
 		return "/member/addMember02";
 	}
 
--- src/main/java/amdn/anywhere/service/MemberService.java
+++ src/main/java/amdn/anywhere/service/MemberService.java
@@ -1,16 +1,31 @@
 package amdn.anywhere.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import amdn.anywhere.domain.FoodMainCate;
 import amdn.anywhere.domain.Member;
 import amdn.anywhere.mapper.MemberMapper;
 
-
 @Service
-@Transactional
 public class MemberService {
 
-	//소비자, 소상공인 회원 등록
+	private final MemberMapper memberMapper;
 	
+	public MemberService(MemberMapper memberMapper) {
+		this.memberMapper = memberMapper;
+	}
+	
+	//선호-비선호 카테고리 불러오기
+	public List<FoodMainCate> getFoodMainList(){
+		List<FoodMainCate> foodMainList = memberMapper.getFoodMainList();
+		return foodMainList;
+	}
+	
+	//소비자, 소상공인 회원 등록
+	public int addMember02(Member member) {
+		return memberMapper.addMember02(member);
+	}
+
 }
