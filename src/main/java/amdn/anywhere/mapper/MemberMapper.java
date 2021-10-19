a9c4c3543c01ab3b032dbbbaf96b2de7596ab024
Moon <ansdbal7@naver.com>
2021-10-19 22:53:04 +0900

    [포인트 구현중]

--- src/main/java/amdn/anywhere/mapper/MemberMapper.java
+++ src/main/java/amdn/anywhere/mapper/MemberMapper.java
@@ -13,6 +13,12 @@ import amdn.anywhere.domain.MemberUserLike;
 @Mapper
 public interface MemberMapper {
 
+	//소비자(user)회원 탈퇴
+	public int removeMemberUser(String userId);
+	
+	//전체(member)회원탈퇴
+	public int removeMember(String memberId);
+	
 	//선호-비선호 선택 등록
 	public int modifyUserLike(MemberUserLike memberUserLike);
 	
