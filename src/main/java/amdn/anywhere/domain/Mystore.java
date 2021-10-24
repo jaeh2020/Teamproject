b1b0f988759e265124eb31a8b903dc3dc2039595
jaeh <syoto6636@gmail.com>
2021-10-24 21:14:12 +0900

    [10.24]

--- src/main/java/amdn/anywhere/domain/Mystore.java
+++ src/main/java/amdn/anywhere/domain/Mystore.java
@@ -6,11 +6,43 @@ public class Mystore {
 	private String bizCode;
 	private String storeName;
 	private String mainCateCode;
+	private String menuName;
+	private String menuCode;
+	private String menuPrice;
+	private String month;
+	private String expenseItem;
+	private String expenseDetail;
+	private String expensePrice;
+	
 	public String getStoreCode() {
 		return storeCode;
 	}
 	public void setStoreCode(String storeCode) {
 		this.storeCode = storeCode;
+	}
+	public String getMenuCode() {
+		return menuCode;
+	}
+	public String getExpenseItem() {
+		return expenseItem;
+	}
+	public void setExpenseItem(String expenseItem) {
+		this.expenseItem = expenseItem;
+	}
+	public String getExpenseDetail() {
+		return expenseDetail;
+	}
+	public void setExpenseDetail(String expenseDetail) {
+		this.expenseDetail = expenseDetail;
+	}
+	public String getExpensePrice() {
+		return expensePrice;
+	}
+	public void setExpensePrice(String expensePrice) {
+		this.expensePrice = expensePrice;
+	}
+	public void setMenuCode(String menuCode) {
+		this.menuCode = menuCode;
 	}
 	public String getStoreLocation() {
 		return storeLocation;
@@ -36,21 +68,30 @@ public class Mystore {
 	public void setMainCateCode(String mainCateCode) {
 		this.mainCateCode = mainCateCode;
 	}
+	public String getMenuName() {
+		return menuName;
+	}
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+	public String getMenuPrice() {
+		return menuPrice;
+	}
+	public void setMenuPrice(String menuPrice) {
+		this.menuPrice = menuPrice;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Mystore [storeCode=");
-		builder.append(storeCode);
-		builder.append(", storeLocation=");
-		builder.append(storeLocation);
-		builder.append(", bizCode=");
-		builder.append(bizCode);
-		builder.append(", storeName=");
-		builder.append(storeName);
-		builder.append(", mainCateCode=");
-		builder.append(mainCateCode);
-		builder.append("]");
-		return builder.toString();
+		return "Mystore [storeCode=" + storeCode + ", storeLocation=" + storeLocation + ", bizCode=" + bizCode
+				+ ", storeName=" + storeName + ", mainCateCode=" + mainCateCode + ", menuName=" + menuName
+				+ ", menuCode=" + menuCode + ", menuPrice=" + menuPrice + ", month=" + month + ", expenseItem="
+				+ expenseItem + ", expenseDetail=" + expenseDetail + ", expensePrice=" + expensePrice + "]";
 	}
 	
 }
